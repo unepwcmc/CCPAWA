63d84e404df3b0a54116228a835498aa433ac8bb
James Cox <th3james@gmail.com>
2010-12-17 14:14:14 +0000

    First commit of created rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CCPAWA::Application.config.secret_token = '59521dc6e4a3cdbb4aa37ca7dd5a215e9675bb1f2503577d2bd7954da69ed8a71e6e90ddebcf818a1297e1a844f3ded0811226bd57c360f6b811d83409709eea'
