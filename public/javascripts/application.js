fb38cbaa916fad151944f9656fc820aadfb04176
James Cox <th3james@gmail.com>
2010-12-22 16:40:51 +0000

    Basic tree structure

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -18,14 +18,14 @@ $(document).ready(function()
       "json_data" : { 
         "ajax" : {
           // the URL to fetch the data
-          "url" : "/docs.json",
+          "url" : "/docs.js",
           // this function is executed in the instance's scope (this refers to the tree instance)
           // the parameter is the node being loaded (may be -1, 0, or undefined when loading the root nodes)
           "data" : function (n) { 
             // the result is fed to the AJAX request `data` option
             return { 
               "operation" : "get_children", 
-              "id" : n.attr ? n.attr("id").replace("node_","") : 1 
+              "id" : n.attr ? n.attr("id") : 1 
             }; 
           }
         }
