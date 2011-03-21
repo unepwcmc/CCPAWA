9c05e988190104524a159c26c56f4df6800c4b72
James Cox <th3james@gmail.com>
2011-03-21 14:37:16 +0000

    Green theme added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :set_locale
+  before_filter :set_theme
 
   def set_locale
     # if params[:locale] is nil then I18n.default_locale will be used
@@ -8,10 +9,17 @@ class ApplicationController < ActionController::Base
     I18n.locale = :fr if host == "ccapao"
     I18n.locale = params[:locale]
   end
+
+  def set_theme
+    @theme = 'global'
+    if ['global','green'].include? params[:theme]
+      @theme = params[:theme]
+    end
+  end
     
   def default_url_options(options={})
     logger.debug "default_url_options is passed options: #{options.inspect}\n"
-    { :locale => I18n.locale }
+    { :locale => I18n.locale, :theme => @theme }
   end
     
 end
