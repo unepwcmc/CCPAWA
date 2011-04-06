aeac06e5d60d02d7c9ad924af0b159ab1ab666bc
James Cox <th3james@gmail.com>
2011-04-06 16:43:44 +0100

    Re ordered people page

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,7 +2,7 @@ class PeopleController < ApplicationController
   # GET /people
   # GET /people.xml
   def index
-    @people = Person.all
+    @people = Person.order("name ASC").all
 
     respond_to do |format|
       format.html # index.html.erb
