7e872a609a0eefd59ada5e03a03483c8745be67f
James Cox <th3james@gmail.com>
2011-10-17 15:41:03 +0100

    Removed deploy borking lines

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
-$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
-require "rvm/capistrano"                  # Load RVM's capistrano plugin.
-set :rvm_ruby_string, 'ruby-1.9.2-p136'        # Or whatever env you want it to run in.
+#$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+#require "rvm/capistrano"                  # Load RVM's capistrano plugin.
+#set :rvm_ruby_string, 'ruby-1.9.2-p136'        # Or whatever env you want it to run in.
 require "bundler/capistrano"
 set :bundle_flags,    "--without test"
 #
